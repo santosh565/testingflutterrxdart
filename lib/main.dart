0b24ea928694149e912a8786d6a3aa4c5557732a
hsotnas76 <hsotnas76@gmail.com>
2022-09-13 07:51:25 +0545

    step-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:rxdart/rxdart.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'TestingFlutterRxDart',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
@@ -19,49 +22,34 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends HookWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    //crete behavior subject every time widget is re-built
+    final subject = useMemoized(() => BehaviorSubject<String>(), [key]);
+
+    // dispose of the old subject every time widget is rebuilt
+    useEffect(() => subject.close, [subject]);
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: StreamBuilder<String>(
+          stream: subject.stream
+              .distinct()
+              .debounceTime(const Duration(seconds: 1)),
+          initialData: 'Please start typing...',
+          builder: (context, snapshot) {
+            return Text(snapshot.requireData);
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      body: Center(
+          child: TextField(
+        onChanged: subject.sink.add,
+      )),
     );
   }
 }
